0b55f1303b3d42844aae7f35d4d0ebfba24d17ab
Alexey Ignatyev <AlexeyIgnatyev@coherentsolutions.com>
2017-05-31 11:39:16 +0300

    step3

--- js/mainPlugin.js
+++ js/mainPlugin.js
@@ -1,17 +1,27 @@
 (function($){
 	let data = {};
+	let self;
 	jQuery.fn.comboInit = function(options){
+		self = this[0];
 		data = options;
 		let optionString = '';
+		let valueToSet = '';
+		let flag = false;
+
 		_.each(data.options, (element) => {
 			optionString += '<option value=' + element.value + '>' + element.content + '</option>';
+			if (!flag && element.content.trim() === data.selectedValue.trim()){
+				valueToSet = data.selectedValue;
+				flag = true;
+			}
 		});
 		
-		$(this).append(optionString);
+		$(self).append(optionString);
+		jQuery.fn.setValue(valueToSet);
 	}
 
 	jQuery.fn.getValue = function(){
-		let activeElem = _.find(this[0], (element) => {
+		let activeElem = _.find(self, (element) => {
 			return element.selected === true;
 		});
 
@@ -22,13 +32,13 @@
 		console.log(necessaryElem);
 	};
 
-	jQuery.fn.setValue = function() {
+	jQuery.fn.setValue = function(valueToSet) {
 		let isElementExist = _.find(data.options, (element) => {
-			return element.content.trim() === data.selectedValue.trim();
+			return element.content.trim() === valueToSet.trim();
 		});
 
 		if (isElementExist){
-			let elementToSet = _.find(this[0].childNodes, (element) => {
+			let elementToSet = _.find(self.childNodes, (element) => {
 				return $(element).val() == isElementExist.value;
 			});
 			$(elementToSet).attr('selected', 'selected');
